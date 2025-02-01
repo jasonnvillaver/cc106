9c9184ff4e3260d0f04b48136b4ca7b4cc20c77a
Jasonn Villaver <jvillaver1423@gmail.com>
2025-02-01 09:15:20 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let name = "John";
+let age = 25;
+let isStudent = true;
+console.log(`Name: ${name}, Age: ${age}, Student: ${isStudent}`);
+
+for (let i = 1; i <= 10; i++) {
+ console.log(i);
+}
+
+function addNumbers(a, b) {
+ return a + b;
+}
+console.log(addNumbers(5, 10));
+
+if (age >= 18) {
+ console.log("Adult");
+} else {
+ console.log("Minor");
+}
